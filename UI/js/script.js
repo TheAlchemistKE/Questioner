aeda1e0dc34ad4a5562607b8e84c22bd4d5c6118
KelynPNjeri <kelynpaul20.kp@gmail.com>
2019-01-04 18:28:43 +0300

    [Feature #162982314]Create a toggle function for the hamburger menu.

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,9 +1,11 @@
 const menu = document.querySelector('.navlinks');
 const menuToggle = document.querySelector('#menu');
 
+// Toggling display on click of the menu button.
 menuToggle.addEventListener('click', (e) => {
   e.preventDefault();
 
+  // Checking the style state.
   if (window.getComputedStyle(menu).display === 'block') {
     menu.style.display = 'none';
   } else if (window.getComputedStyle(menu).display === 'flex') {
